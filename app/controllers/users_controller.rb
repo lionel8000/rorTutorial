5d11181037ef99f5823db58a54123a46f5bb0944
lionel8000 <8836272@qq.com>
2011-12-15 14:40:27 +0800

    part index finished need go on

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
-  before_filter :authenticate,:only=>[:edit,:update]
+  before_filter :authenticate,:only=>[:index,:edit,:update]
   before_filter :correctuser,:only=>[:edit,:update]
+  
+  def index
+    @title = "All USERS"
+    @users = User.all 
+  end
+  
   def show
     @user = User.find(params[:id])
     @title = "SHow lionel's user"
